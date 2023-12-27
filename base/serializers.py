691026dcdaf5868b8ac0bf4bd6338fd123d5128b
Ethan <stayhungry134@gmail.com>
2023-12-27 14:18:16 +0800

    feat: 删除系列化基类

--- base/serializers.py
+++ base/serializers.py
@@ -14,21 +14,4 @@ class BaseModelSerializer(serializers.ModelSerializer):
     """
     序列化的基类
     """
-    def __init__(self, model=None, fields=None, **kwargs):
-        self.Meta.model = model
-        if fields:
-            self.Meta.fields = fields
-        elif hasattr(model, 'serializable_fields'):
-            self.Meta.fields = model.serializable_fields
-        else:
-            self.Meta.fields = '__all__'
-        super().__init__()
-
-    class Meta:
-        content_type = 'application/json'
-
-    def save(self, **kwargs):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
+    pass
